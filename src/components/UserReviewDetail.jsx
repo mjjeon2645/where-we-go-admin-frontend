5e35240a2649143bade0b42d1b825e0105b58b69
Jeon Minji <mjjeon2645@gmail.com>
2022-12-05 13:51:42 +0900

    리뷰 상세정보 페이지 UI 재설정 및 방문장소 항목 추가

--- src/components/UserReviewDetail.jsx
+++ src/components/UserReviewDetail.jsx
@@ -5,6 +5,129 @@ const Container = styled.div`
     padding: 5em;
 `;
 
+const Title = styled.p`
+  font-size: 1.2em;
+  font-weight: bold;
+  margin-block: 1em;
+`;
+
+const Wrapper = styled.article`
+  padding-inline: 1em;
+  margin-bottom: 3em;
+`;
+
+const ReviewId = styled.section`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding-block: 1.2em;
+  border-top: 1px solid #EEE;
+
+  p:first-child {
+    font-weight: bold;
+  }
+
+  span {
+    color: #6c6c6c;
+  }
+`;
+
+const Nickname = styled.section`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding-block: 1.2em;
+  border-top: 1px solid #EEE;
+
+  p:first-child {
+    font-weight: bold;
+  }
+
+  span {
+    color: #6c6c6c;
+  }
+`;
+
+const DateOfWritten = styled.section`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding-block: 1.2em;
+  border-block: 1px solid #EEE;
+
+  p:first-child {
+    font-weight: bold;
+  }
+
+  p:last-child {
+    color: #6c6c6c;
+  }
+`;
+
+const DateOfVisit = styled.section`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding-block: 1.2em;
+  border-block: 1px solid #EEE;
+
+  p:first-child {
+    font-weight: bold;
+  }
+
+  p:last-child {
+    color: #6c6c6c;
+  }
+`;
+
+const PlaceName = styled.section`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding-block: 1.2em;
+  border-bottom: 1px solid #EEE;
+
+  p:first-child {
+    font-weight: bold;
+  }
+
+  p:last-child {
+    color: #6c6c6c;
+  }  
+`;
+
+const Rate = styled.section`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding-block: 1.2em;
+  border-bottom: 1px solid #EEE;
+
+  p:first-child {
+    font-weight: bold;
+  }
+
+  span {
+    color: #6c6c6c;
+  }
+`;
+
+const Body = styled.section`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding-block: 1.2em;
+  border-bottom: 1px solid #EEE;
+
+  p:first-child {
+    font-weight: bold;
+  }
+
+  span {
+    color: #6c6c6c;
+  }
+`;
+
 export default function UserReviewDetail({ userReview, toggleModal, goToPrevPage }) {
   const handleOpenModalClick = () => {
     toggleModal();
@@ -18,45 +141,39 @@ export default function UserReviewDetail({ userReview, toggleModal, goToPrevPage
     <Container>
       {Object.keys(userReview).length ? (
         <div>
-          <table>
-            <tr>
-              <th>리뷰 고유번호</th>
-              <td />
-              <td>{userReview.id}</td>
-            </tr>
-            <tr>
-              <th>작성자 닉네임</th>
-              <td />
-              <td>{userReview.nickname}</td>
-            </tr>
-            <tr>
-              <th>작성일</th>
-              <td />
-              <td>{dateFormatter(userReview.createdAt)}</td>
-            </tr>
-            <tr>
-              <th>방문일</th>
-              <td />
-              <td>{dateFormatter(userReview.dateOfVisit)}</td>
-            </tr>
-            <tr>
-              <th>방문 장소</th>
-              <td />
-              <td>장소 이름 들어가야함~</td>
-            </tr>
-            <tr>
-              <th>평점</th>
-              <td />
-              <td>{userReview.rate}</td>
-            </tr>
-            <tr>
-              <th>내용</th>
-              <td />
-              <td>{userReview.body}</td>
-            </tr>
-          </table>
-          <button type="button" onClick={handleOpenModalClick}>삭제하기</button>
-          <button type="button" onClick={handlePageBackClick}>뒤로가기</button>
+          <Title>리뷰 상세정보</Title>
+          <Wrapper>
+            <ReviewId>
+              <p>리뷰 고유번호</p>
+              <p>{userReview.id}</p>
+            </ReviewId>
+            <Nickname>
+              <p>작성자 닉네임</p>
+              <span>{userReview.nickname}</span>
+            </Nickname>
+            <DateOfWritten>
+              <p>작성일</p>
+              <p>{dateFormatter(userReview.createdAt)}</p>
+            </DateOfWritten>
+            <DateOfVisit>
+              <p>방문일</p>
+              <p>{dateFormatter(userReview.dateOfVisit)}</p>
+            </DateOfVisit>
+            <PlaceName>
+              <p>방문 장소</p>
+              <p>{userReview.placeName}</p>
+            </PlaceName>
+            <Rate>
+              <p>평점</p>
+              <p>{userReview.rate}</p>
+            </Rate>
+            <Body>
+              <p>내용</p>
+              <p>{userReview.body}</p>
+            </Body>
+            <button type="button" onClick={handleOpenModalClick}>삭제하기</button>
+            <button type="button" onClick={handlePageBackClick}>뒤로가기</button>
+          </Wrapper>
         </div>
       ) : (
         <p>now loading...</p>
